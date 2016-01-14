f7582c0aa3207c0a6104974c310f39960e818d90
haachicanoy <harold22010@gmail.com>
2016-01-14 09:56:50 -0500

    Some changes

--- updateRegressionFun.R
+++ updateRegressionFun.R
@@ -1,5 +1,5 @@
 
-dirServ <- "D:/GIT_HUB_REPOSITORIOS/RAW_REGRESSION_MODELS_BIGDATA/"
+dirServ <- "C:/Users/haachicanoy/Documents/GitHub/RAW_REGRESSION_MODELS_BIGDATA"
 
 lisFil <- list.files(dirServ,full.names = T,pattern = ".R",recursive = T)
 lisFil <- lisFil[-grep("updateRegressionFun",lisFil)]
@@ -7,4 +7,5 @@ lisFil <- lisFil[-grep("multiProfileFix",lisFil)]
 
 lapply(lisFil,source)
 
-save(list = ls(all.names = TRUE), file = "//dapadfs/workspace_cluster_6/TRANSVERSAL_PROJECTS/MADR/COMPONENTE_2/OPEN_BIGDATA_AEPS/REGRESSION_MODELS/All-Functions-AEPS_BD.RData")
+save(list = ls(all.names = TRUE), file = "C:/Users/haachicanoy/Documents/GitHub/RAW_REGRESSION_MODELS_BIGDATA/All-Functions-AEPS_BD.RData")
+# "//dapadfs/workspace_cluster_6/TRANSVERSAL_PROJECTS/MADR/COMPONENTE_2/OPEN_BIGDATA_AEPS/REGRESSION_MODELS/All-Functions-AEPS_BD.RData"
